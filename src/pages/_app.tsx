432c757cf8f0948a1f60c888a80f008840a99a8a
Shadyar <sh.utegenov@vlife.kz>
2022-10-25 18:28:42 +0600

    Remove and added CssBaseline

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
-import '../../styles/resetcss.css'
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '@fontsource/nunito/300.css'
 import '@fontsource/nunito/400.css'
 import '@fontsource/nunito/500.css'
 import '@fontsource/nunito/700.css'
+import { CssBaseline } from '@mui/material'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
