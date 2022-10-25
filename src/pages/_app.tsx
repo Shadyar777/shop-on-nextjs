5f123efb653e246c15f7e473b4a6b675f74ae1d7
Shadyar <sh.utegenov@vlife.kz>
2022-10-25 18:26:00 +0600

    Added font and fixed imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../styles/resetcss.css'
-import '../styles/globals.css'
+import '../../styles/resetcss.css'
+import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import '@fontsource/nunito/300.css'
+import '@fontsource/nunito/400.css'
+import '@fontsource/nunito/500.css'
+import '@fontsource/nunito/700.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
