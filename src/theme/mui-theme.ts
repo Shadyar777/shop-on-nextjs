2489b70703f95703a8bb0ca659cccbab9f1ca6c7
Shadyar <sh.utegenov@vlife.kz>
2022-11-05 22:12:11 +0600

    chore: Set colors for the project

--- src/theme/mui-theme.ts
+++ src/theme/mui-theme.ts
@@ -4,118 +4,30 @@ import { createTheme, ThemeOptions } from '@mui/material/styles'
 export const tokens = (mode: string) => ({
   ...(mode === 'dark'
     ? {
-        grey: {
-          100: '#e0e0e0',
-          200: '#c2c2c2',
-          300: '#a3a3a3',
-          400: '#858585',
-          500: '#666666',
-          600: '#525252',
-          700: '#3d3d3d',
-          800: '#292929',
-          900: '#141414',
-        },
-        primary: {
-          100: '#d0d1d5',
-          200: '#a1a4ab',
-          300: '#727681',
-          400: '#1F2A40',
-          500: '#141b2d',
-          600: '#101624',
-          700: '#0c101b',
-          800: '#080b12',
-          900: '#040509',
-        },
-        greenAccent: {
-          100: '#dbf5ee',
-          200: '#b7ebde',
-          300: '#94e2cd',
-          400: '#70d8bd',
-          500: '#4cceac',
-          600: '#3da58a',
-          700: '#2e7c67',
-          800: '#1e5245',
-          900: '#0f2922',
-        },
-        redAccent: {
-          100: '#f8dcdb',
-          200: '#f1b9b7',
-          300: '#e99592',
-          400: '#e2726e',
-          500: '#db4f4a',
-          600: '#af3f3b',
-          700: '#832f2c',
-          800: '#58201e',
-          900: '#2c100f',
-        },
-        blueAccent: {
-          100: '#e1e2fe',
-          200: '#c3c6fd',
-          300: '#a4a9fc',
-          400: '#868dfb',
-          500: '#6870fa',
-          600: '#535ac8',
-          700: '#3e4396',
-          800: '#2a2d64',
-          900: '#151632',
-        },
+        blue: '#0466c8',
+        background: 'white',
+        darkBlue: '#03045e',
+        lightBlue: '#304a58',
+        oceanBlue: '#48cae4',
+        blueBackground: '#caf0f8',
+        cardColor: 'rgb(241, 240, 240)',
+        cardColor2: 'white',
+        red: '#f94144',
+        svgColor: 'black',
+        elevateShadow: 'rgba(0, 0, 0, 0.25) 0px 25px 50px -12px',
       }
     : {
-        grey: {
-          100: '#141414',
-          200: '#292929',
-          300: '#3d3d3d',
-          400: '#525252',
-          500: '#666666',
-          600: '#858585',
-          700: '#a3a3a3',
-          800: '#c2c2c2',
-          900: '#e0e0e0',
-        },
-        primary: {
-          100: '#040509',
-          200: '#080b12',
-          300: '#0c101b',
-          400: '#f2f0f0', // manually changed
-          500: '#141b2d',
-          600: '#1F2A40',
-          700: '#727681',
-          800: '#a1a4ab',
-          900: '#d0d1d5',
-        },
-        greenAccent: {
-          100: '#0f2922',
-          200: '#1e5245',
-          300: '#2e7c67',
-          400: '#3da58a',
-          500: '#4cceac',
-          600: '#70d8bd',
-          700: '#94e2cd',
-          800: '#b7ebde',
-          900: '#dbf5ee',
-        },
-        redAccent: {
-          100: '#2c100f',
-          200: '#58201e',
-          300: '#832f2c',
-          400: '#af3f3b',
-          500: '#db4f4a',
-          600: '#e2726e',
-          700: '#e99592',
-          800: '#f1b9b7',
-          900: '#f8dcdb',
-        },
-        blueAccent: {
-          100: '#151632',
-          200: '#2a2d64',
-          300: '#3e4396',
-          400: '#535ac8',
-          500: '#6870fa',
-          600: '#868dfb',
-          700: '#a4a9fc',
-          800: '#c3c6fd',
-          900: '#e1e2fe',
-        },
+        blue: '#0466c8',
+        background: 'black',
+        darkBlue: '#8eecf5',
+        lightBlue: '#4895ef',
+        oceanBlue: '#12131d',
+        blueBackground: '#12131d',
+        cardColor: '#12131d',
+        cardColor2: '#12131d',
+        red: '#12131d',
+        svgColor: '#8eecf5',
+        elevateShadow: 'rgba(32, 29, 233, 0.637) 0px 25px 50px -12px',
       }),
 })
 export type PaletteMode = 'light' | 'dark'
@@ -130,7 +42,7 @@ export const themeSettings = (mode: string): ThemeOptions => {
         sm: 600,
         md: 900,
         lg: 1200,
-        xl: 1536,
+        xl: 1600,
       },
     },
     palette: {
@@ -139,35 +51,47 @@ export const themeSettings = (mode: string): ThemeOptions => {
         ? {
             // palette values for dark mode
             primary: {
-              main: colors.primary[500],
+              main: colors.blue,
             },
-            secondary: {
-              main: colors.greenAccent[500],
-            },
+            // secondary: {
+            //   main: colors.greenAccent[500],
+            // },
             neutral: {
-              dark: colors.grey[700],
-              main: colors.grey[500],
-              light: colors.grey[100],
+              primary: colors.cardColor,
+              secondary: colors.cardColor2,
+            },
+            text: {
+              primary: colors.darkBlue,
+              secondary: colors.lightBlue,
             },
             background: {
-              default: colors.primary[500],
+              default: colors.background,
+            },
+            custom: {
+              shadow: colors.elevateShadow,
             },
           }
         : {
             // palette values for light mode
             primary: {
-              main: colors.primary[100],
+              main: colors.blue,
             },
-            secondary: {
-              main: colors.greenAccent[500],
-            },
+            // secondary: {
+            //   main: colors.greenAccent[500],
+            // },
             neutral: {
-              dark: colors.grey[700],
-              main: colors.grey[500],
-              light: colors.grey[100],
+              primary: colors.cardColor,
+              secondary: colors.cardColor2,
+            },
+            text: {
+              primary: colors.darkBlue,
+              secondary: colors.lightBlue,
             },
             background: {
-              default: '#fcfcfc',
+              default: colors.background,
+            },
+            custom: {
+              shadow: colors.elevateShadow,
             },
           }),
     },
@@ -180,15 +104,15 @@ export const themeSettings = (mode: string): ThemeOptions => {
       },
       h2: {
         fontFamily: ['Nunito', 'sans-serif'].join(','),
-        fontSize: 32,
+        fontSize: '3.5rem',
       },
       h3: {
         fontFamily: ['Nunito', 'sans-serif'].join(','),
-        fontSize: 24,
+        fontSize: '2rem',
       },
       h4: {
         fontFamily: ['Nunito', 'sans-serif'].join(','),
-        fontSize: 20,
+        fontSize: '2rem',
       },
       h5: {
         fontFamily: ['Nunito', 'sans-serif'].join(','),
